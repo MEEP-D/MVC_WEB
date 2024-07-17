5812b4876d003bc27dc521197e345eda5b862582
MEEP-D <acduong4567@gmail.com>
2024-07-17 22:31:56 +0700

    Add project files.

---
+++ MVC_WEB/Models/Course.cs
@@ -0,0 +1,28 @@
+namespace MVC_WEB.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Course()
+        {
+            Enrollments = new HashSet<Enrollment>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ MVC_WEB/Models/DBSchoolsContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MVC_WEB.Models
+{
+    public partial class DBSchoolsContext : DbContext
+    {
+        public DBSchoolsContext()
+            : base("name=DBSchoolsConnectionString")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Enrollment> Enrollments { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
